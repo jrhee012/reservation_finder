9976c0d9e217f47cc2b030eda861a28dda1f9552
Jaehyuk Rhee <jaehyuk.rhee@gmail.com>
2018-11-15 17:11:17 -0500

    update work thread script

--- server/workers/index.js
+++ server/workers/index.js
@@ -15,22 +15,9 @@ function startWorker(path, cb) {
 
 if (isMainThread) {
     console.log('This is the main thread')
-
-    // let w = new Worker(__filename, { workerData: 111 });
-    // w.on('message', (msg) => { //A message from the worker!
-    //     console.log('received message from worker!');
-    //     console.log(msg.text);
-    // })
-    // w.on('error', console.error);
-    // w.on('exit', code => {
-    //     if (code != 0)
-    //         console.error(new Error(`Worker stopped with exit code ${code}`))
-    // });
-
     let w = startWorker(__filename, console.log);
-    // console.log('w ', w);
     console.log('waiting for worker!');
-} else { //the worker's code
+} else {
     console.log(workerData)
     console.log('sending to parent!');
     setTimeout(() => parentPort.postMessage({ text: 'hi!' }), 5000);
